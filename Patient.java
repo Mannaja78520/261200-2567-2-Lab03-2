1574096e8d79566a7731a32f35eded3ccd8d7b08
mannaja <manman7852078520@gmail.com>
2024-12-04 15:49:27 +0700

    update lab03_02

--- Patient.java
+++ Patient.java
@@ -1,24 +1,17 @@
 public class Patient {
     private long id;
     private String name;
-    private int birthYear;
+    private int birthYear = 2000;
     private double height;
     private double weight;
     private String bloodGroup;
     private String phoneNumber;
-
-    private int defaultBirthYear = 2000;
-    private double defaultHeight = 150.00;
-    private double defaultWeight = 50.00;
     
     public Patient(long id, String name, int birthYear, double height, double weight, String bloodGroup, String phoneNumber) {
         this.id = id;
         this.name = name;
 
-        if (birthYear < 0) {
-            System.out.println("Invite birthday input change to defaultBirthYear.");
-            this.birthYear = this.defaultBirthYear;
-        }
+        if (birthYear < 0) System.out.println("Invite birthday input, Change to defaultBirthYear.");
         else this.birthYear = birthYear;
         
         if (height < 0) System.out.println("Invalid height input.");
